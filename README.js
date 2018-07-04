8890efcabf0d1234f96305b80764a5a40fd8dc01
paoyuchin <bow1130bow@gmail.com>
2018-07-04 17:52:36 +0800

    tag

--- README.js
+++ README.js
@@ -1,10 +1,4 @@
-<html>
-      <head>
-        <meta charset="UTF-8">
-        <title>README.md</title>
-      </head>
-      <body>
-        <p>$(&#39;.frzTable.default&#39;).frzTable({
+$('.frzTable.default').frzTable({
     count: {
         // M版時每次點擊往前往後移動幾格儲存格
         slide: 1, // [number] 
@@ -14,10 +8,7 @@
     // 設定花多久時間移動完成
     speed: .3, // [number] 
     // 每次點擊儲存格時會執行此callback，並帶入所點擊的儲存格jquery物件
-    whenClick: function($element) {
+    whenClick: function ($element) {
         // console.log($element)
     }
-})</p>
-
-      </body>
-    </html>
+})
