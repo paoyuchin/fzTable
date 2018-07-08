a56973d7f62619b4356a26944a18ac949f92a217
paoyuchin <bow1130bow@gmail.com>
2018-07-08 17:31:45 +0800

    update

--- dist/module.js
+++ dist/module.js
@@ -180,7 +180,7 @@ module.exports = g;
 
 
 Object.defineProperty(exports, "__esModule", {
-    value: true
+  value: true
 });
 
 var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
@@ -192,112 +192,117 @@ var ModuleName = "frztable";
 
 //Props default value write here
 var ModuleDefaults = {
-    count: {
-        // M版時每次點擊往前往後移動幾格儲存格
-        slide: 3, // [number]
-        // M版時一個畫面show幾格儲存格
-        show: 3 // [number]
-    },
-    // 設定花多久時間移動完成
-    speed: .3, // [number]
-    // 每次點擊儲存格時會執行此callback，並帶入所點擊的儲存格jquery物件
-    whenClick: function whenClick($element) {
-        console.log($element);
-    }
+  count: {
+    // M版時每次點擊往前往後移動幾格儲存格
+    slide: 3, // [number]
+    // M版時一個畫面show幾格儲存格
+    show: 3 // [number]
+  },
+  // 設定花多久時間移動完成
+  speed: 0.3, // [number]
+  // 每次點擊儲存格時會執行此callback，並帶入所點擊的儲存格jquery物件
+  whenClick: function whenClick($element) {
+    console.log($element);
+  }
 };
 
 //Define you want to get function returns from outside of scope
 var ModuleReturns = []; //string
 
 function onMouseClick(event) {
-    var $this = $(this);
-    var _ref = [$this.data("forth"), $this.data("back")],
-        f = _ref[0],
-        b = _ref[1];
+  var $this = $(this);
+  var _ref = [$this.data("forth"), $this.data("back")],
+      f = _ref[0],
+      b = _ref[1];
 
-    $this.removeClass("selected");
-    $('.frztable_date').removeClass("add_selected_bg selected");
-    $this.addClass("selected");
-    $(".frztable_date[data-forth=" + f + "]").addClass("add_selected_bg");
-    $(".frztable_date[data-back=" + b + "]").addClass("add_selected_bg");
-    console.log("f:", f, "b:", b);
-    console.log(event.data.whenClickCallback($this));
+  $this.removeClass("selected");
+  $(event.data.target + "_date").removeClass("add_selected_bg selected");
+  $this.addClass("selected");
+  $(event.data.target + "_date[data-forth=" + f + "]").addClass("add_selected_bg");
+  $(event.data.target + "_date[data-back=" + b + "]").addClass("add_selected_bg");
+  console.log("f:", f, "b:", b);
+  console.log(event.data.whenClickCallback($this));
 }
 
-function onMouseOver() {
-    $(".frztable_date").removeClass("mousover_effect");
-    $(this).addClass("mousover_effect");
+function onMouseOver(event) {
+  console.log(event.data.target);
+  $(event.data.target + "_date").removeClass("mousover_effect");
+  $(this).addClass("mousover_effect");
 }
 
 function slide(dir) {
-    var change = this.option.count.slide;
-    var op = '';
-    if (dir === "left") {
-        if (this.firstCol - change < 0) {
-            console.log("right overflow");
-            change = this.firstCol; //最後要移動的值，等於this.firstCol的距離，(左邊的虛線框框)
-        }
-        op = '+=';
-        this.firstCol -= change;
-    } else if (dir === "right") {
-        if (this.firstCol + change >= 7 - this.option.count.show) {
-            console.log("left overflow");
-            change = 7 - this.option.count.show - this.firstCol;
-        }
-        op = '-=';
-        this.firstCol += change;
+  var change = this.option.count.slide;
+  var op = "";
+  if (dir === "left") {
+    if (this.firstCol - change < 0) {
+      console.log("right overflow");
+      change = this.firstCol; //最後要移動的值，等於this.firstCol的距離，(左邊的虛線框框)
     }
-    $(".frztable_overflow").animate({
-        left: op + change * this.colWidth
-    }, this.option.speed * 1000);
-    console.log(this.firstCol);
+    op = "+=";
+    this.firstCol -= change;
+  } else if (dir === "right") {
+    if (this.firstCol + change >= 7 - this.option.count.show) {
+      console.log("left overflow");
+      change = 7 - this.option.count.show - this.firstCol;
+    }
+    op = "-=";
+    this.firstCol += change;
+  }
+  $(this.target + "_overflow").animate({
+    left: op + change * this.colWidth
+  }, this.option.speed * 1000);
+  console.log(this.firstCol);
 } //slide
 
 function onResize() {
-    var winWidth = $(window).width();
-    this.colWidth = (winWidth - $(".frztable_col-1").width()) / this.option.count.show;
-    // this.colWidth -= 1;
-    if (winWidth <= 980) {
-        console.log($(".frztable_column").css("width"));
-        console.log("-----------");
-        console.log('overflow:' + $(".frztable_overflow").css("width"));
-        $(".frztable_column").css("width", this.colWidth);
-        $(".frztable_overflow").css("width", this.colWidth * 7);
-    } else {
-        $(".frztable_column").css("width", this.tmp1);
-        $(".frztable_overflow").css("width", this.tmp2);
-    }
+  var winWidth = $(window).width();
+  this.colWidth = (winWidth - $(this.target + "_col-1").width()) / this.option.count.show;
+  // this.colWidth -= 1;
+  if (winWidth <= 980) {
+    console.log($(this.target + "_column").css("width"));
+    console.log("-----------");
+    console.log("overflow:" + $(this.target + "_overflow").css("width"));
+    $(this.target + "_column").css("width", this.colWidth);
+    $(this.target + "_overflow").css("width", this.colWidth * 7);
+  } else {
+    $(this.target + "_column").css("width", this.tmp1);
+    $(this.target + "_overflow").css("width", this.tmp2);
+  }
 } //
 
 var Module = function () {
-    function Module(ele, options) {
-        _classCallCheck(this, Module);
+  function Module(ele, options) {
+    _classCallCheck(this, Module);
 
-        this.option = options;
-        this.$btnLeft = $(".frztable_btn-left");
-        this.$btnRight = $(".frztable_btn-right");
-        this.$cell = $(".frztable_date");
-        this.firstCol = 0;
-    }
+    this.target = ".frztable";
+    this.option = options;
+    this.$btnLeft = $(this.target + "_btn-left");
+    this.$btnRight = $(this.target + "_btn-right");
+    this.$cell = $(this.target + "_date");
+    this.firstCol = 0;
+  }
 
-    _createClass(Module, [{
-        key: "init",
-        value: function init() {
-            this.$cell.click({
-                whenClickCallback: this.option.whenClick
-            }, onMouseClick);
-            this.$cell.mouseover(onMouseOver);
-            this.tmp1 = $(".frztable_column").css("width");
-            this.tmp2 = $(".frztable_overflow").css("width");
-            $(window).resize(onResize.bind(this));
-            $(window).resize();
-            this.$btnLeft.click(slide.bind(this, "left")); //btnLeft
-            this.$btnRight.click(slide.bind(this, "right")); //btnLeft
-        } //run first here
+  _createClass(Module, [{
+    key: "init",
+    value: function init() {
+      this.$cell.click({
+        whenClickCallback: this.option.whenClick,
+        target: this.target
+      }, onMouseClick);
+      this.$cell.mouseover({
+        target: this.target
+      }, onMouseOver);
+      this.tmp1 = $(this.target + "_column").css("width");
+      this.tmp2 = $(this.target + "_overflow").css("width");
+      $(window).resize(onResize.bind(this));
+      $(window).resize();
+      this.$btnLeft.click(slide.bind(this, "left")); //btnLeft
+      this.$btnRight.click(slide.bind(this, "right")); //btnLeft
+    } //run first here
 
-    }]);
+  }]);
 
-    return Module;
+  return Module;
 }();
 
 exports.ModuleName = ModuleName;
--- script/module.js
+++ script/module.js
@@ -3,107 +3,118 @@ const ModuleName = "frztable";
 
 //Props default value write here
 const ModuleDefaults = {
-    count: {
-        // M版時每次點擊往前往後移動幾格儲存格
-        slide: 3, // [number]
-        // M版時一個畫面show幾格儲存格
-        show: 3 // [number]
-    },
-    // 設定花多久時間移動完成
-    speed: .3, // [number]
-    // 每次點擊儲存格時會執行此callback，並帶入所點擊的儲存格jquery物件
-    whenClick: function ($element) {
-        console.log($element);
-    }
+  count: {
+    // M版時每次點擊往前往後移動幾格儲存格
+    slide: 3, // [number]
+    // M版時一個畫面show幾格儲存格
+    show: 3 // [number]
+  },
+  // 設定花多久時間移動完成
+  speed: 0.3, // [number]
+  // 每次點擊儲存格時會執行此callback，並帶入所點擊的儲存格jquery物件
+  whenClick: function($element) {
+    console.log($element);
+  }
 };
 
 //Define you want to get function returns from outside of scope
 const ModuleReturns = []; //string
 
 function onMouseClick(event) {
-    let $this = $(this);
-    let [f, b] = [$this.data("forth"), $this.data("back")];
-    $this.removeClass("selected");
-    $('.frztable_date').removeClass("add_selected_bg selected");
-    $this.addClass("selected");
-    $(".frztable_date[data-forth=" + f + "]").addClass("add_selected_bg");
-    $(".frztable_date[data-back=" + b + "]").addClass("add_selected_bg");
-    console.log("f:", f, "b:", b);
-    console.log(event.data.whenClickCallback($this));
+  let $this = $(this);
+  let [f, b] = [$this.data("forth"), $this.data("back")];
+  $this.removeClass("selected");
+  $(event.data.target + "_date").removeClass("add_selected_bg selected");
+  $this.addClass("selected");
+  $(event.data.target + "_date[data-forth=" + f + "]").addClass(
+    "add_selected_bg"
+  );
+  $(event.data.target + "_date[data-back=" + b + "]").addClass(
+    "add_selected_bg"
+  );
+  console.log("f:", f, "b:", b);
+  console.log(event.data.whenClickCallback($this));
 }
 
-function onMouseOver() {
-    $(".frztable_date").removeClass("mousover_effect");
-    $(this).addClass("mousover_effect");
+function onMouseOver(event) {
+  console.log(event.data.target);
+  $(event.data.target + "_date").removeClass("mousover_effect");
+  $(this).addClass("mousover_effect");
 }
 
 function slide(dir) {
-    let change = this.option.count.slide;
-    let op = '';
-    if (dir === "left") {
-        if (this.firstCol - change < 0) {
-            console.log("right overflow");
-            change = this.firstCol; //最後要移動的值，等於this.firstCol的距離，(左邊的虛線框框)
-        }
-        op = '+='
-        this.firstCol -= change;
-    } else if (dir === "right") {
-        if (this.firstCol + change >= 7 - this.option.count.show) {
-            console.log("left overflow");
-            change = 7 - this.option.count.show - this.firstCol;
-        }
-        op = '-='
-        this.firstCol += change;
+  let change = this.option.count.slide;
+  let op = "";
+  if (dir === "left") {
+    if (this.firstCol - change < 0) {
+      console.log("right overflow");
+      change = this.firstCol; //最後要移動的值，等於this.firstCol的距離，(左邊的虛線框框)
     }
-    $(".frztable_overflow").animate({
-            left: op + change * this.colWidth
-        },
-        this.option.speed * 1000
-    );
-    console.log(this.firstCol);
+    op = "+=";
+    this.firstCol -= change;
+  } else if (dir === "right") {
+    if (this.firstCol + change >= 7 - this.option.count.show) {
+      console.log("left overflow");
+      change = 7 - this.option.count.show - this.firstCol;
+    }
+    op = "-=";
+    this.firstCol += change;
+  }
+  $(this.target + "_overflow").animate(
+    {
+      left: op + change * this.colWidth
+    },
+    this.option.speed * 1000
+  );
+  console.log(this.firstCol);
 } //slide
 
 function onResize() {
-    let winWidth = $(window).width();
-    this.colWidth =
-        (winWidth - $(".frztable_col-1").width()) / this.option.count.show;
-    // this.colWidth -= 1;
-    if (winWidth <= 980) {
-        console.log($(".frztable_column").css("width"));
-        console.log("-----------");
-        console.log('overflow:' + $(".frztable_overflow").css("width"));
-        $(".frztable_column").css("width", this.colWidth);
-        $(".frztable_overflow").css("width", this.colWidth * 7);
-    } else {
-        $(".frztable_column").css("width", this.tmp1);
-        $(".frztable_overflow").css("width", this.tmp2);
-    }
+  let winWidth = $(window).width();
+  this.colWidth =
+    (winWidth - $(this.target + "_col-1").width()) / this.option.count.show;
+  // this.colWidth -= 1;
+  if (winWidth <= 980) {
+    console.log($(this.target + "_column").css("width"));
+    console.log("-----------");
+    console.log("overflow:" + $(this.target + "_overflow").css("width"));
+    $(this.target + "_column").css("width", this.colWidth);
+    $(this.target + "_overflow").css("width", this.colWidth * 7);
+  } else {
+    $(this.target + "_column").css("width", this.tmp1);
+    $(this.target + "_overflow").css("width", this.tmp2);
+  }
 } //
 class Module {
-    constructor(ele, options) {
-        this.option = options;
-        this.$btnLeft = $(".frztable_btn-left");
-        this.$btnRight = $(".frztable_btn-right");
-        this.$cell = $(".frztable_date");
-        this.firstCol = 0;
-    }
+  constructor(ele, options) {
+    this.target = ".frztable";
+    this.option = options;
+    this.$btnLeft = $(this.target + "_btn-left");
+    this.$btnRight = $(this.target + "_btn-right");
+    this.$cell = $(this.target + "_date");
+    this.firstCol = 0;
+  }
 
-    init() {
-        this.$cell.click({
-            whenClickCallback: this.option.whenClick
-        }, onMouseClick);
-        this.$cell.mouseover(onMouseOver);
-        this.tmp1 = $(".frztable_column").css("width");
-        this.tmp2 = $(".frztable_overflow").css("width");
-        $(window).resize(onResize.bind(this));
-        $(window).resize();
-        this.$btnLeft.click(slide.bind(this, "left")); //btnLeft
-        this.$btnRight.click(slide.bind(this, "right")); //btnLeft
-    } //run first here
+  init() {
+    this.$cell.click(
+      {
+        whenClickCallback: this.option.whenClick,
+        target: this.target
+      },
+      onMouseClick
+    );
+    this.$cell.mouseover(
+      {
+        target: this.target
+      },
+      onMouseOver
+    );
+    this.tmp1 = $(this.target + "_column").css("width");
+    this.tmp2 = $(this.target + "_overflow").css("width");
+    $(window).resize(onResize.bind(this));
+    $(window).resize();
+    this.$btnLeft.click(slide.bind(this, "left")); //btnLeft
+    this.$btnRight.click(slide.bind(this, "right")); //btnLeft
+  } //run first here
 }
-export {
-    ModuleName,
-    ModuleDefaults,
-    ModuleReturns,
-    Module
-};
+export { ModuleName, ModuleDefaults, ModuleReturns, Module };
